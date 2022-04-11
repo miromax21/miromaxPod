eb0f1fc50ece3aa0fa549bc9216e58f9977c6402
maxim.mironov <maxim.mironov@cifrasoft.com>
2022-04-11 12:21:11 +0400

    Initial Commit

---
+++ EventSDK/EventSDK.h
@@ -0,0 +1,18 @@
+//
+//  EventSDK.h
+//  EventSDK
+//
+//  Created by Sergey Zhidkov on 11.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EventSDK.
+FOUNDATION_EXPORT double EventSDKVersionNumber;
+
+//! Project version string for EventSDK.
+FOUNDATION_EXPORT const unsigned char EventSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventSDK/PublicHeader.h>
+
+
