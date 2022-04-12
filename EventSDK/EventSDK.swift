36a2d0ee912e1649cc699c57b481a132fb4b0fb4
maxim.mironov <maxim.mironov@cifrasoft.com>
2022-04-12 11:42:42 +0400

    fsdfdfsdf

---
+++ EventSDK/EventSDK.swift
@@ -0,0 +1,14 @@
+//
+//  EventSDK.swift
+//  EventSDK
+//
+//  Created by Sergey Zhidkov on 11.04.2022.
+//
+
+import Foundation
+open class EventSDK {
+    public init (){}
+    public func getId() -> String{
+        return "EventSDK"
+    }
+}
