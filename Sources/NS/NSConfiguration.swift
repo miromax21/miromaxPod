c27a7d6be1d028d6381a48c232bfe7e3cdf10a52
maxim.mironov <maxim.mironov@cifrasoft.com>
2022-06-02 16:41:03 +0400

    + NS

---
+++ Sources/NS/NSConfiguration.swift
@@ -0,0 +1,27 @@
+//
+//  NSConfiguration.swift
+//  EventSDK
+//
+//  Created by Sergey Zhidkov on 02.06.2022.
+//
+
+import Foundation
+public class NSConfiguration: NSObject, ConfigurationType{
+  public var cid: String!
+  
+  public var tms: String!
+  
+  public var hid: String?
+  
+  public var uid: String?
+  
+  public var uidc: Int?
+    
+  @objc public init(cid: String, tms: String!, uid: String?, hid: String?, uidc: NSNumber?) {
+    super.init()
+    self.cid = cid
+    self.tms = tms
+    self.hid = hid
+    self.uidc = uidc as? Int
+  }
+}
---
+++ Sources/NS/NSEventSDK.swift
@@ -0,0 +1,37 @@
+//
+//  NSEventSDK.swift
+//  EventSDK
+//
+//  Created by Sergey Zhidkov on 02.06.2022.
+//
+
+import Foundation
+public class NSEventSDK: NSObject{
+  private var eventSdk: EventSDK!
+  @objc public init(configuration: NSConfiguration){
+    super.init()
+    eventSdk = EventSDK(configuration: configuration)
+  }
+
+  @objc public func next(contactType: Int, view: NSNumber?, idc: NSNumber?, idlc: String,  fts: Int64,  urlc: String,  media: String, ver: NSNumber?) {
+    var eventView: EventType? = nil
+    if let view = view {
+      eventView = EventType(rawValue: Int(truncating: view))
+    }
+    eventSdk.next(Event(contactType: ContactType(rawValue: Int(contactType)), view: eventView, idc: idc as? Int, idlc: idlc, fts: fts, urlc: urlc, media: media, ver: ver as? Int))
+  }
+
+  @objc public func getSendingQueue() -> Array<String>{
+    return eventSdk.sendingQueue.compactMap {$0}
+  }
+  
+  @objc public func getUserAttributes() -> NSMutableDictionary  {
+    let nsAttributes: NSMutableDictionary = [:]
+    eventSdk.userAttributes.forEach{
+      if let next = $0.first{
+        nsAttributes.setObject(next.value, forKey: next.key as NSCopying)
+      }
+    }
+    return nsAttributes
+  }
+}
